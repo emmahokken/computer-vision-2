99194592a9211f04d5173764ea3264b5bfbc7c36
emmahokken <emmahokken@gmail.com>
2020-04-14 14:41:22 +0200

    std fix

--- assignment_1/Code/main.py
+++ assignment_1/Code/main.py
@@ -8,6 +8,8 @@ import pickle as pkl
 import sys
 import argparse
 import time
+from tqdm import tqdm
+plt.style.use('seaborn')
 
 from data import read_pcd
 
@@ -71,8 +73,9 @@ def compute_R_t(base, target):
 
     return R, t
 
-def iterative_closest_point(base, target, stacked):
+def iterative_closest_point(base, target, stacked, i):
     all_RMS = []
+    iters = []
 
     no_points = int(target.shape[0]*ARGS.sub_sampling_r)
     target  = sub_sampling(target, target[0:no_points])
@@ -83,11 +86,13 @@ def iterative_closest_point(base, target, stacked):
     # Optimize R and t using the EM-algorithm
     RMS = math.inf
     for iter in range(ARGS.max_icp_iters): #todo: maybe chance to a while loop.
+        iters.append(iter)
         if ARGS.sub_sampling_method == 'random':
             base_sub = sub_sampling(base, target)
 
         ind, new_RMS = find_closest_points(base_sub, target) # E-step
         RMS = new_RMS
+        #print(RMS)
         all_RMS.append(RMS)
         R, t = compute_R_t(base_sub, target[ind,:]) # M-step
 
@@ -97,11 +102,22 @@ def iterative_closest_point(base, target, stacked):
 
         stacked = np.dot(R, stacked.T).T - t.T
 
-        print(RMS)
 
-        if iter > 2 and abs(all_RMS[-1] - all_RMS[-2]) < ARGS.icp_treshold:
-            break
+        if iter > ARGS.icp_treshold_w:
+            std = np.std(all_RMS[-ARGS.icp_treshold_w:])
+            if std < ARGS.icp_treshold:
+                break
 
+
+    # # Plot iteration
+    # if i == 0:
+    #     plt.plot(iters, all_RMS)
+    #     plt.xlabel('iteration')
+    #     plt.ylabel('RMS')
+    #     plt.savefig('../results/rms_icp_{}_{}_{}.png'
+    #                 .format(ARGS.sub_sampling_method, ARGS.sub_sampling_r,
+    #                         ARGS.step_size))
+    #     plt.clf()
 
     return stacked, RMS
 
@@ -113,47 +129,67 @@ def merge_pcds():
     base = read_pcd('../Data/data/0000000000.pcd') #load first base
     stacked = base
     RMSs = []
+    iters = []
 
     start_time = time.time()
 
-    for i in range(ARGS.start, ARGS.end, ARGS.step_size):
-        print(f'iteration {i}')
+    for i in tqdm(range(ARGS.start, ARGS.end, ARGS.step_size)):
         target = read_pcd(f'../Data/data/00000000{i + 1:02}.pcd') #load target
-        stacked, RMS = iterative_closest_point(base, target, stacked) #update base
-        stacked = np.vstack((stacked, target))
-        base = target
+
+        stacked_bu = np.copy(stacked) #back-up stacked
+        stacked, RMS = iterative_closest_point(base, target, stacked, i) #transform stacked
+
+        if RMS < 0.004:
+            stacked = np.vstack((stacked, target))
+            base = target
+        else:
+            print(i, RMS)
+            stacked = stacked_bu
+
         RMSs.append(RMS)
+        iters.append(i)
 
     pkl.dump(base, open('../results/{}_{}_{}.pkl'
                 .format(ARGS.sub_sampling_method, ARGS.sub_sampling_r,
                         ARGS.step_size), "wb"))
 
-    print(f'execution time: {time.time() - start_time} seconds')
+
 
     av_RMS = np.mean(RMSs)
+    print(f'execution time: {time.time() - start_time} seconds')
+    print('average RMS: {}'.format(av_RMS))
+
+    plt.plot(iters, RMSs)
+    plt.xlabel('point cloud pair')
+    plt.ylabel('RMS')
+    plt.savefig('../results/rms_main_{}_{}_{}.png'
+                .format(ARGS.sub_sampling_method, ARGS.sub_sampling_r,
+                        ARGS.step_size))
+    plt.clf()
+
     visualize_pcd(stacked)
-
-
 
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser()
     PARSER.add_argument('--start', default=0, type=int,
                         help='first pcd')
-    PARSER.add_argument('--end', default=10, type=int,
+    PARSER.add_argument('--end', default=99, type=int,
                         help='final pcd')
     PARSER.add_argument('--step_size', default=1, type=int,
                         help='step size between the pcds')
 
     PARSER.add_argument('--sub_sampling_method', default='uniform', type=str,
                         help='method for sub sampling pcd rows, uniform or random')
-    PARSER.add_argument('--sub_sampling_r', default=0.1, type=float,
+    PARSER.add_argument('--sub_sampling_r', default=0.5, type=float,
                         help='ratio for sub sampling')
 
-    PARSER.add_argument('--max_icp_iters', default=100, type=int,
+    PARSER.add_argument('--max_icp_iters', default=1000, type=int,
                         help='max number of iterations for icp algorithm')
-    PARSER.add_argument('--icp_treshold', default=0.000001, type=float,
+    PARSER.add_argument('--icp_treshold', default=0.00001, type=float,
                         help='treshold for early stopping icp algorithm')
+    PARSER.add_argument('--icp_treshold_w', default=10, type=int,
+                        help='window for treshold for icp algorithm')
 
     ARGS = PARSER.parse_args()
 
