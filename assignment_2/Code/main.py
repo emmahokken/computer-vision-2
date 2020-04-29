87c27f9f2519a24a3948cb38aad315b8b92ac529
emmahokken <emmahokken@gmail.com>
2020-04-29 11:39:52 +0200

    find matches

--- assignment_2/Code/main.py
+++ assignment_2/Code/main.py
@@ -1,35 +1,44 @@
 import numpy as np 
-import cv2 
+import cv2 as cv
 from matplotlib import pyplot as plt
 from sklearn import linear_model
 
+GOOD_MATCH_PERCENT = 0.15
 
 
 def main():
 
     # read image and make it greyscale
-    img = cv2.imread('Data/House/frame00000001.png')
-    grey = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img1 = cv.imread('Data/House/frame00000001.png')
+    grey1 = cv.cvtColor(img1, cv.COLOR_RGB2GRAY)
 
+    img2 = cv.imread('Data/House/frame00000035.png')
+    grey2 = cv.cvtColor(img2, cv.COLOR_RGB2GRAY)
 
-    # perform sift
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp = sift.detect(grey, None)
+    # perform sift to get keypoints and descriptors 
+    sift = cv.xfeatures2d.SIFT_create()
+    kp1, des1 = sift.detectAndCompute(grey1, None)
+    kp2, des2 = sift.detectAndCompute(grey2, None)
 
-    # not entirally sure what this does
-    kp, des = sift.compute(grey, kp)
+    # match descriptors (using L1 norm now, because it's the only one that worked...)
+    matcher = cv.BFMatcher(cv.NORM_L1, crossCheck=False)
+    matches = matcher.match(des1, des2, None)
+
+    # only keep good matcjes (why? idk)
+    matches.sort(key=lambda x: x.distance, reverse=False)
+    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+    matches = matches[:numGoodMatches]
     
+    # draw top matches
+    imMatches = cv.drawMatches(img1, kp1, img2, kp2, matches, None)
+    cv.imwrite("Data/SIFT/matches.jpg", imMatches)
+        
     # draw keypoints on original image and save it
-    img= cv2.drawKeypoints(grey, kp, img)
-    cv2.imwrite('Data/SIFT/sift_keypoints.jpg',img)
+    img= cv.drawKeypoints(grey1, kp1, img1)
+    cv.imwrite('Data/SIFT/other_sift_keypoints.jpg',img)
 
-
-
-
-    # do RANSAC
-    ransac = linear_model.RANSACRegressor()
-    # ransac.fit(X, y)
-
+    # RANSAC 
+    
 
 
 if __name__ == "__main__":
