edeba4e4bf132ba8280a832fa6df95174afa6355
tim142 <t1i4m2@gmail.com>
2020-05-01 14:11:55 +0200

    fix data path

--- assignment_2/Code/main.py
+++ assignment_2/Code/main.py
@@ -1,16 +1,14 @@
-import numpy as np 
-import cv2 
+import numpy as np
+import cv2
 from matplotlib import pyplot as plt
 from sklearn import linear_model
 
-
+import sys
 
 def main():
-
     # read image and make it greyscale
-    img = cv2.imread('Data/House/frame00000001.png')
+    img = cv2.imread('../Data/House/frame00000001.png')
     grey = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
 
     # perform sift
     sift = cv2.xfeatures2d.SIFT_create()
@@ -18,13 +16,10 @@ def main():
 
     # not entirally sure what this does
     kp, des = sift.compute(grey, kp)
-    
+
     # draw keypoints on original image and save it
     img= cv2.drawKeypoints(grey, kp, img)
     cv2.imwrite('Data/SIFT/sift_keypoints.jpg',img)
-
-
-
 
     # do RANSAC
     ransac = linear_model.RANSACRegressor()
